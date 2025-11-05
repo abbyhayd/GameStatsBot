3c25b549b366ad13984fad527d1df6eade1f1929
abbyhayd <abigayle.doinog@gmail.com>
2025-11-04 16:25:36 -0800

    added basic gemini prompting in hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print("hello world")
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+client = genai.Client()
+
+prompt = "Who won the 2025 World Series?"
+prompt += "And also provide the final score of game 7 of the 2025 World Series."
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=prompt
+)
+
+if response != None:
+    print(response.text)
+else:
+    print("broke")
