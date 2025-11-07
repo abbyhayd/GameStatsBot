0163809bdb29f81214b75cc4134d3bc7c80d43ab
abbyhayd <abigayle.doinog@gmail.com>
2025-11-06 18:44:35 -0800

    separated commands.py to events.py and fortnite.py

---
+++ cogs/events.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'We have logged in as {self.bot.user}')
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        if message.content.startswith('!hello'):
+            await message.channel.send("hello!")
--- cogs/fortnite.py
+++ cogs/fortnite.py
@@ -1,24 +1,6 @@
-
-from discord.ext import commands
-
 from dotenv import load_dotenv
-
+from discord.ext import commands
 import fortnite_api
-
-class Events(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(f'We have logged in as {self.bot.user}')
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
-        if message.content.startswith('!hello'):
-            await message.channel.send("hello!")
 
 class FortniteCog(commands.Cog):
 
@@ -43,5 +25,3 @@ class FortniteCog(commands.Cog):
                 f"The total number of cosmetics in Fortnite is: {total_cosmetics}",
                 ephemeral=True,
             )
-
-    
