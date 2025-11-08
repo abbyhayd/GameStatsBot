9ced9c645321b5370e571dbf752a5c78b366e0b4
abbyhayd <abigayle.doinog@gmail.com>
2025-11-08 01:09:01 -0800

    commented out reload in bot_commands.py

--- cogs/bot_commands.py
+++ cogs/bot_commands.py
@@ -13,13 +13,13 @@ class botCommands(commands.Cog):
     async def cog_unload(self):
         print(f"{self.__class__.__name__} unloaded!")
 
-    @commands.command()
-    @commands.is_owner()
-    async def reload(self, ctx, extension):
-        sys.modules[f'./cogs/{extension}.py'] = importlib.reload(sys.modules[f'./cogs/{extension}.py'])
-        await self.bot.reload_extension(f"cogs.{extension}")
-        embed = discord.Embed(title='Reload', description=f'{extension} successfully reloaded', color=0xff00c8)
-        await ctx.send(embed=embed)
+    # @commands.command()
+    # @commands.is_owner()
+    # async def reload(self, ctx, extension):
+    #     sys.modules[f'./cogs/{extension}.py'] = importlib.reload(sys.modules[f'./cogs/{extension}.py'])
+    #     await self.bot.reload_extension(f"cogs.{extension}")
+    #     embed = discord.Embed(title='Reload', description=f'{extension} successfully reloaded', color=0xff00c8)
+    #     await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(botCommands(bot))
