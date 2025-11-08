631f4a15bf0d4710c024351baa6f54256636a6e4
abbyhayd <abigayle.doinog@gmail.com>
2025-11-07 23:09:17 -0800

    reload work in bot_commands.py

--- cogs/bot_commands.py
+++ cogs/bot_commands.py
@@ -1,28 +1,19 @@
 from discord.ext import commands
 import discord
+import sys
+import importlib
 
 class botCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-    # @commands.command()
-    # @commands.is_owner()
-    # async def reload(self, ctx, extension):
-    #     await self.bot.reload_extension(f"cogs.{extension}")
-    #     embed = discord.Embed(title='Reload', description=f'{extension} successfully reloaded', color=0xff00c8)
-    #     await ctx.send(embed=embed)
-
-    @commands.command(name='reload', hidden=True)
+    @commands.command()
     @commands.is_owner()
-    async def _reload(self, *, module : str):
-        try:
-            self.bot.unload_extension(module)
-            self.bot.load_extension(module)
-        except Exception as e:
-            await self.bot.say('\N{PISTOL}')
-            await self.bot.say('{}: {}'.format(type(e).__name__, e))
-        else:
-            await self.bot.say('\N{OK HAND SIGN}')
+    async def reload(self, ctx, extension):
+        sys.modules[f'./cogs/{extension}.py'] = importlib.reload(sys.modules[f'./cogs/{extension}.py'])
+        await self.bot.reload_extension(f"cogs.{extension}")
+        embed = discord.Embed(title='Reload', description=f'{extension} successfully reloaded', color=0xff00c8)
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(botCommands(bot))
